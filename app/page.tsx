34bf555779e03fb18f5ea1cf1ed5cf3f8cbc274a
Rodrigo <rdesouza2008@gmaill.com>
2025-11-17 16:40:00 -0300

    feature(shadcn-ui): add card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Header from "@/components/Header";
+import ProductCard from "@/components/ProductCard/ProductCard";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <main>
+      <Header />
+      <ProductCard />
+    </main>
+  );
 }
