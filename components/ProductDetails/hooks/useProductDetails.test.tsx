e04ae7341b619ce6b3ebb8842095cf6ceea9c16b
Rodrigo <rdesouza2008@gmaill.com>
2025-11-22 09:50:12 -0300

    write tests for useProductDetails

---
+++ components/ProductDetails/hooks/useProductDetails.test.tsx
@@ -0,0 +1,48 @@
+import { productsService } from "@/lib/services/products.service";
+import { Product } from "@/types/products";
+import { useProductDetails } from "./useProductDetails";
+
+jest.mock("@/lib/services/products.service");
+
+const mockedGetById = productsService.getById as jest.Mock;
+
+describe("useProductDetails", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return product when api response is success", async () => {
+    const mockProduct: Product = {
+      id: 1,
+      title: "Test Product",
+      description: "Testing...",
+      price: 10,
+      category: "category1",
+      image: "www.example.com/image.png",
+    };
+
+    mockedGetById.mockResolvedValue(mockProduct);
+
+    const response = await useProductDetails({ productId: 1 });
+
+    expect(productsService.getById).toHaveBeenCalledWith(1);
+
+    expect(response).toEqual({
+      product: mockProduct,
+      error: false,
+    });
+  });
+
+  it("should return {error: true} when api call fails", async () => {
+    mockedGetById.mockRejectedValue(new Error("Erro"));
+
+    const response = await useProductDetails({ productId: 1 });
+
+    expect(productsService.getById).toHaveBeenCalledWith(1);
+
+    expect(response).toEqual({
+      product: null,
+      error: true,
+    });
+  });
+});
