535725b0f40f5cd6eef27ab125e4e0efa9ca6dba
Rodrigo <rdesouza2008@gmaill.com>
2025-11-23 08:43:54 -0300

    fix: remove ';'

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginContainer from "./LoginContainer";
 export default function LoginPage() {
   return (
     <main className="max-w-[1400px] m-auto p-4 pt-8">
-      <LoginContainer />;
+      <LoginContainer />
     </main>
   );
 }
