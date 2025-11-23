c27b047f96e0c1a4f03bd02e3f05bf90669b4800
Rodrigo <rdesouza2008@gmaill.com>
2025-11-22 21:20:31 -0300

    chore: adjust axios timeout

--- lib/api.ts
+++ lib/api.ts
@@ -2,5 +2,5 @@ import axios, { AxiosInstance } from 'axios';
 
 export const api: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || '',
-  timeout: 3000,
+  timeout: 10000,
 });
