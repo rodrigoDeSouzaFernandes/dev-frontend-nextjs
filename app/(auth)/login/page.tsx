685ded4672a389632384a4356a50ab981978e249
Rodrigo <rdesouza2008@gmaill.com>
2025-11-22 22:33:42 -0300

    fix: fix sign up tooltip on mobile devices

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
 import LoginContainer from "./LoginContainer";
 
 export default function LoginPage() {
-  return <LoginContainer />;
+  return (
+    <main className="max-w-[1400px] m-auto p-4 pt-8">
+      <LoginContainer />;
+    </main>
+  );
 }
